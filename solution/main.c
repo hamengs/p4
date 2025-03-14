31b6d8b53e1e08361ed96ac169eaaaabc0b8edfd
hamengs <wangzhengyao1107@outlook.com>
2025-03-14 18:15:47 -0500

    In main.c separately do kinit2 to make sure khuge page has its own virtual space

--- solution/main.c
+++ solution/main.c
@@ -38,7 +38,8 @@ main(void)
   fileinit();      // file table
   ideinit();       // disk 
   startothers();   // start other processors
-  kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
+  kinit2(P2V(4*1024*1024), (void*)HUGE_PAGE_VSTART); // must come after startothers()
+  kinit2((void*)HUGE_PAGE_VEND, P2V(PHYSTOP)); // rest of physical memory
   khugeinit2((void*)HUGE_PAGE_VSTART, (void*)HUGE_PAGE_VEND); // huge page allocator
   userinit();      // first user process
   mpmain();        // finish this processor's setup
