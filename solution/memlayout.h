e464377084b8953d6291123681d82cbb25f6f2c7
hamengs <wangzhengyao1107@outlook.com>
2025-03-14 18:30:35 -0500

    I'm tired

--- solution/memlayout.h
+++ solution/memlayout.h
@@ -1,7 +1,7 @@
 // Memory layout
 
 #define EXTMEM  0x100000            // Start of extended memory
-#define PHYSTOP 0x80000000          // Top physical memory （Changed to 2GB for huge pages）
+#define PHYSTOP 0xE000000          // Top physical memory （Changed to 2GB for huge pages）
 
 #define DEVSPACE 0xFE000000         // Other devices are at high addresses
 
