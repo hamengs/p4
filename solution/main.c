56ba3931d60badeb694336e217b447cc9e3014b5
hamengs <wangzhengyao1107@outlook.com>
2025-03-14 18:23:37 -0500

    switch kvmalloc and khugeinit1 order

--- solution/main.c
+++ solution/main.c
@@ -23,8 +23,8 @@ main(void)
 {
   kinit1(end, P2V(4*1024*1024)); // phys page allocator
   cprintf("After kinit1: end = %p\n", end);
-  khugeinit1((void*)HUGE_PAGE_VSTART, (void*)HUGE_PAGE_VEND); // huge page allocator
   kvmalloc();      // kernel page table
+  khugeinit1((void*)HUGE_PAGE_VSTART, (void*)HUGE_PAGE_VEND); // huge page allocator
   mpinit();        // detect other processors
   lapicinit();     // interrupt controller
   seginit();       // segment descriptors
