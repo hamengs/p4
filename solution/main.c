8ddb49eeb474d11ffc733be18f204350329999dd
hamengs <wangzhengyao1107@outlook.com>
2025-03-14 21:12:47 -0500

    kinit2 in main.c file changed

--- solution/main.c
+++ solution/main.c
@@ -36,8 +36,7 @@ main(void)
   fileinit();      // file table
   ideinit();       // disk 
   startothers();   // start other processors
-  kinit2(P2V(4*1024*1024), (void*)HUGE_PAGE_VSTART); // must come after startothers()
-  kinit2((void*)HUGE_PAGE_VEND, P2V(PHYSTOP)); // rest of physical memory
+  kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
   khugeinit((void*)HUGE_PAGE_VSTART, (void*)HUGE_PAGE_VEND); // huge page allocator
   userinit();      // first user process
   mpmain();        // finish this processor's setup
